9be2475b700ec13a7bae948fa08db663dc8cd907
Ossmar Manuel Martínez Elizarraraz <81430843+PanicDark@users.noreply.github.com>
2021-04-22 15:58:25 -0500

    Interfaz de problemas

---
+++ problemas.php
@@ -0,0 +1,73 @@
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <table class="table">
+            <thead>
+                <th scope="col">EJECUTIVO</th>
+                <th scope="col">PROBLEMA</th>
+                <th scope="col">ESTATUS</th>
+                <th scope="col">FECHA Y HORA</th>
+                <th scope="col">RESPONSABLE</th>
+            </thead>
+            <tbody>
+                <tr class="table-danger">
+                    <td>Cristian</td>
+                    <td>La PC esta demasiado lenta</td>
+                    <td>Pendiente</td>
+                    <td>21/04/2021 12:00 PM</td>
+                    <td>
+                        <select name="" id="" class="form-select form-select-sm" aria-label=".form-select-sm example">
+                            <option value="">SELECCIONA</option>
+                        </select>
+                    </td>
+                    <td class="text-aling center"><button class="btn btn-outline-danger">ARREGLAR</button></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table class="table">
+            <thead>
+                <th scope="col">EJECUTIVO</th>
+                <th scope="col">PROBLEMA</th>
+                <th scope="col">ESTATUS</th>
+                <th scope="col">FECHA Y HORA</th>
+                <th scope="col">RESPONSABLE</th>
+            </thead>
+            <tbody>
+                <tr class="table-warning">
+                    <td>Cristian</td>
+                    <td>La PC esta demasiado lenta</td>
+                    <td>En Proceso</td>
+                    <td>21/04/2021 12:10 PM</td>
+                    <td>Marco Antonio Delgadillo Bautista</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table class="table">
+            <thead>
+                <th scope="col">EJECUTIVO</th>
+                <th scope="col">PROBLEMA</th>
+                <th scope="col">FECHA Y HORA PENDIENTE</th>
+                <th scope="col">FECHA Y HORA EN PROCESO</th>
+                <th scope="col">FECHA Y HORA FINALIZADO</th>
+                <th scope="col">RESPONSABLE</th>
+                <th scope="col">COMENTARIO</th>
+            </thead>
+            <tbody>
+                <tr class="table-success">
+                    <td>Cristian</td>
+                    <td>La PC esta demasiado lenta</td>
+                    <td>21/04/2021 12:00 PM</td>
+                    <td>21/04/2021 12:10 PM</td>
+                    <td>21/04/2021 12:40 PM</td>
+                    <td>Marco Antonio Delgadillo Bautista</td>
+                    <td>Ninguno</td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
