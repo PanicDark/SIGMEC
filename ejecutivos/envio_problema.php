91dec104a4d27b6aaee988460f759cb3e1446e09
PanicDark <ossmar161810_omme@outlook.com>
2021-04-22 16:58:11 -0500

    Agregación de carpetas para ejecutivos y administradores

---
+++ ejecutivos/envio_problema.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    </head>
+    <body class="container">
+    <form action="">
+        <div class="form-floating mb-3">
+            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                <option selected>SELECCIONA</option>
+                <option value="1">Activación de la licencia de Office</option>
+                <option value="2"></option>
+                <option value="3">Three</option>
+            </select>
+            <label for="floatingSelect">Problema</label>
+        </div>
+        <div class="form-floating mb-3">
+            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                <option selected>SELECCIONA</option>
+                <option value="1">Mayra</option>
+                <option value="2">Daniel</option>
+                <option value="3">Francisco</option>
+            </select>
+            <label for="floatingSelect">Ejecutivo</label>
+        </div>
+    </form>
+    </body>
+</html>
