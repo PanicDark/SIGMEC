be873833093fbccb68b16b27db60c05bbd8018b2
LizParraL <2519160107lparral@gmail.com>
2021-04-22 15:54:17 -0500

    Se ingresa el diseño del inicio de sesión

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIGMEC</title>
+    <link rel="shortcut icon" href="https://img.icons8.com/ios-filled/50/000000/computer-support.png" type="image/x-icon">
+</head>
+<body>
+    <a href="inicio_sesion.php">Iniciar sesión</a>
+</body>
+</html>
---
+++ inicio_sesion.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIGMEC</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="shortcut icon" href="https://img.icons8.com/ios-filled/50/000000/computer-support.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/inicio_sesion.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col align-self-start">
+            <h2>SIGMEC</h2>
+            <p id="leyenda">Sistema Interno para la Gesti&oacute;n del Manteniento de Equipo de Computo</p>
+            <br>
+        </div>
+    </div>
+    <div class="row">
+        <form>
+            <div class="form-group usuario">
+                <!-- <label for="txtUsuario">Usuario</label> -->
+                <input type="email" class="form-control" id="txtUsuario" aria-describedby="emailHelp" placeholder="Ingresa tu usuario">
+            </div>
+            <br>
+            <div class="form-group contrasenia">
+                <!-- <label for="txtContrasenia">Contraseña</label> -->
+                <input type="password" class="form-control" id="txtContrasenia" placeholder="Ingresa tu contraseña">
+            </div>
+            <br>
+            <div class="row">
+                <div class="col btn">
+                <button type="submit" class="btn btn-primary ingresar">Ingresar</button>
+                </div>
+            </div>
+            
+        </form>
+    
+    
+    </div>
+</div>
+
+
+
+
+
+
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+</html>
